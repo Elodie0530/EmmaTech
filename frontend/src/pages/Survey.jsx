b0af98de88f4f1588a7c991a5cb4479625b1d807
Alex-5110 <alexpom@live.fr>
2023-06-29 09:10:00 +0200

    survey.jsx added

--- frontend/src/pages/Survey.jsx
+++ frontend/src/pages/Survey.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
 
-export default QuestionComponent;
+// export default QuestionComponent;
