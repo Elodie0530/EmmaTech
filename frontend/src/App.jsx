8d3bd9fcc4b8c652fb2d53c71b29c6d9aed3c76b
Alex-5110 <alexpom@live.fr>
2023-06-29 09:46:08 +0200

    router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import Home from "./pages/Home";
 import Survey from "./pages/Survey";
 import Android from "./pages/Android";
 import Ios from "./pages/Ios";
-import Formulaire1 from "./components/Formulaire1";
-import Formulaire2 from "./components/Formulaire2";
-import Formulaire3 from "./components/Formulaire3";
+// import Formulaire1 from "./components/Formulaire1";
+// import Formulaire2 from "./components/Formulaire2";
+// import Formulaire3 from "./components/Formulaire3";
 
 function App() {
   return (
@@ -18,9 +18,9 @@ function App() {
         <Route path="/ajoute-un-appareil" element={<Survey />} />
         <Route path="/android" element={<Android />} />
         <Route path="/ios" element={<Ios />} />
-        <Route path="/formulaire1" element={<Formulaire1 />} />
+        {/* <Route path="/formulaire1" element={<Formulaire1 />} />
         <Route path="/formulaire2" element={<Formulaire2 />} />
-        <Route path="formulaire3" element={<Formulaire3 />} />
+        <Route path="formulaire3" element={<Formulaire3 />} /> */}
       </Routes>
     </div>
   );
