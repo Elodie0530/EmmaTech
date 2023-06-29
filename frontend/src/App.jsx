6f45ea99bdcc60c667936d2982945367c3dfd02f
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-06-29 20:26:21 +0200

    mise à jour

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Survey from "./pages/Survey";
-import Android from "./pages/Android";
+
 import Ios from "./pages/Ios";
 import Stockage from "./pages/Stockage";
 import Faq from "./pages/Faq";
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/accueil" element={<Home />} />
         <Route path="/ajoute-un-appareil" element={<Survey />} />
-        <Route path="/android" element={<Android />} />
+
         <Route path="/ios" element={<Ios />} />
         <Route path="/stockage" element={<Stockage />} />
         <Route path="/foire-aux-questions" element={<Faq />} />
--- frontend/src/components/Formulaire2.jsx
+++
@@ -1,3 +0,0 @@
-export default function Formulaire2() {
-  return <> </>;
-}
